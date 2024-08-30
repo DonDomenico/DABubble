faf6f66f13020e401bc589b80fa37892bddd63cb
DonDomenico <d.grunow@hotmail.de>
2024-08-30 11:58:44 +0200

    added route to login-component; added 'export' to firebase-credentials

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,8 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { firebaseCredentials } from './firebase-credentials';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp(firebaseCredentials)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp(firebaseCredentials)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()), provideAnimationsAsync(), provideAnimationsAsync()]
 };
