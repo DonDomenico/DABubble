52ac01dc1d2b47f7cd3ae5978abfccf487824c5e
Alicja K <akwiaton@gmail.com>
2024-09-02 12:28:01 +0200

    side-nav, header, general view - working on components

--- src/app/general-view/general-view.component.ts
+++ src/app/general-view/general-view.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { SidenavComponent } from '../sidenav/sidenav.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-general-view',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SidenavComponent, MatSidenavModule],
   templateUrl: './general-view.component.html',
   styleUrl: './general-view.component.scss'
 })
 export class GeneralViewComponent {
+
 
 }
