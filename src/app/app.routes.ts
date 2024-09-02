5e1934f81716a6690b44e5b43fa365467e561e01
Alicja K <akwiaton@gmail.com>
2024-09-02 10:54:07 +0200

    router link - general view added to login comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { GeneralViewComponent } from './general-view/general-view.component';
 
 export const routes: Routes = [
     { path: "", component: LoginComponent },
-    { path: "signup", component: SignupComponent}
+    { path: "signup", component: SignupComponent},
+    { path: "general-view", component: GeneralViewComponent}
 ];
--- src/app/general-view/general-view.component.ts
+++ src/app/general-view/general-view.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-general-view',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './general-view.component.html',
   styleUrl: './general-view.component.scss'
 })
