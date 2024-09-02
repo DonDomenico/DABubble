0f49558facbc4160df6ad8e679a8ae7bd8f8a06a
DonDomenico <d.grunow@hotmail.de>
2024-09-02 15:03:55 +0200

    small changes in styles.scss

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signOut, updateProfile } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +9,15 @@ export class AuthenticationService {
   constructor() { }
 
   firebaseAuth = inject(Auth);
-  auth = getAuth();
-  user = this.auth.currentUser;
 
   async createUser(email: string, username: string, password: string) {
-    return createUserWithEmailAndPassword(this.firebaseAuth, email, password).then((response) => {
+    await createUserWithEmailAndPassword(this.firebaseAuth, email, password).then((response) => {
       updateProfile(response.user, {displayName: username});
     })
   }
 
-  async showCurrentUser() {
-    onAuthStateChanged(this.auth, (user) => {
+  showCurrentUser() {
+    onAuthStateChanged(this.firebaseAuth, (user) => {
       if(user) {
         console.log(user.uid, user.displayName);
       } else {
