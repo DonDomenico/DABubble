05658bd48cb265ed2bef84e56e41f5557b600f05
Alicja K <akwiaton@gmail.com>
2024-09-03 10:31:53 +0200

    working on html & scss of sidenav component

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { ChannelListComponent } from '../channel-list/channel-list.component';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, ChannelListComponent],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
