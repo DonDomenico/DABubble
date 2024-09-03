59abb6188ed00a634d93ed57f8e3997844f6114f
Alicja K <akwiaton@gmail.com>
2024-09-03 15:35:53 +0200

    chat header added

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { SingleMessageComponent } from './single-message/single-message.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [SingleMessageComponent, MatButtonModule, MatIconModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
