0318e1aadf94a12c22b1382a87002b736d805a86
Alicja K <akwiaton@gmail.com>
2024-09-03 11:49:36 +0200

    scss added to side-nav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { ChannelListComponent } from '../channel-list/channel-list.component';
+import { UsersComponent } from '../users/users.component';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule, ChannelListComponent],
+  imports: [MatSidenavModule, ChannelListComponent, UsersComponent],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
