3a439f17d73d09f43ee6717eca12420717e8a7ce
Alicja K <akwiaton@gmail.com>
2024-09-03 13:57:21 +0200

    general view structure added

--- src/app/general-view/general-view.component.ts
+++ src/app/general-view/general-view.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SidenavComponent } from '../sidenav/sidenav.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { ChatComponent } from '../chat/chat.component';
+import { ThreadComponent } from '../thread/thread.component';
 
 @Component({
   selector: 'app-general-view',
   standalone: true,
-  imports: [HeaderComponent, SidenavComponent, MatSidenavModule],
+  imports: [HeaderComponent, SidenavComponent, ThreadComponent, MatSidenavModule, ChatComponent],
   templateUrl: './general-view.component.html',
   styleUrl: './general-view.component.scss'
 })
