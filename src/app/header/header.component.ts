263f9a8c49fcefcb25b49e1423a32deb13335610
Alicja K <akwiaton@gmail.com>
2024-09-12 15:13:40 +0200

    menu dropdown added to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatInputModule, MatIconModule],
+  imports: [MatToolbarModule, MatInputModule, MatIconModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
