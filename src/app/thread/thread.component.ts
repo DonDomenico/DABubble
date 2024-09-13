9e5b3c346e9e14b4cd7f97c88778333097a2e749
Alicja K <akwiaton@gmail.com>
2024-09-13 16:23:03 +0200

    thread header added

--- src/app/thread/thread.component.ts
+++ src/app/thread/thread.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
