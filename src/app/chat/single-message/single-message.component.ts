bd4be58a63cc869eafe18095ee1cacc973517dd2
Alicja K <akwiaton@gmail.com>
2024-09-19 13:14:33 +0200

    single message and chat components HTML, CSS and TS added

--- src/app/chat/single-message/single-message.component.ts
+++ src/app/chat/single-message/single-message.component.ts
@@ -1,6 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
+
+import { fromEvent, timestamp } from 'rxjs';
+
+
 @Component({
   selector: 'app-single-message',
   standalone: true,
@@ -15,7 +19,20 @@ export class SingleMessageComponent {
 
 @Input() message = {
   userName: "Noah Braun",
-  userMessage: "Welche Version ist aktuell von Angular?"
+  userAvatar: "./assets/img/avatar4.svg",
+  userMessage: "Welche Version ist aktuell von Angular?",
+  userTime: "12:00 Uhr",
+  answer: "2 Antworten",
+  lastAnswerTime: "Letzte Antwort 14:56"
 }
+
+// const clickWithTimestamp = fromEvent(document, 'click').pipe(
+//   timestamp()
+// );
+
+// Emits data of type { value: PointerEvent, timestamp: number }
+// clickWithTimestamp.subscribe((data: { value: PointerEvent, timestamp: number }) => {
+//   console.log(data);
+// });
 
 }
