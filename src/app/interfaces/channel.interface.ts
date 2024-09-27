86b8b3e5bcc0f0a0c6d0020a287a11c0219f939f
Alicja K <akwiaton@gmail.com>
2024-09-27 15:22:14 +0200

    channel service set, with firebase connected

--- src/app/interfaces/channel.interface.ts
+++ src/app/interfaces/channel.interface.ts
@@ -1,2 +1,6 @@
-export interface ChannelInterface {
+export interface Channel {
+    docId: string;
+    name: string;
+    description: string;
+    owner: string;
 }
--- src/app/services/channel.service.ts
+++ src/app/services/channel.service.ts
@@ -1,14 +1,52 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
+import { onSnapshot } from "firebase/firestore";
+import { addDoc, collection, doc, Firestore, updateDoc } from '@angular/fire/firestore';
+import { Channel } from '../interfaces/channel.interface';
 @Injectable({
   providedIn: 'root'
 })
 export class ChannelService {
-  private channelNameSource = new BehaviorSubject<string>('');
-  currentChannelName = this.channelNameSource.asObservable();
+  firestore = inject(Firestore);
 
-  changeChannelName(name: string) {
-    this.channelNameSource.next(name);
+  channels: Channel[] = [];
+  // private channelNameSource = new BehaviorSubject<string>('');
+  // currentChannelName = this.channelNameSource.asObservable();
+
+  // changeChannelName(name: string) {
+  //   this.channelNameSource.next(name);
+  // }
+
+  unsubChannelList;
+
+  constructor() {
+    this.unsubChannelList = this.subChannelList();
+  }
+
+  ngOnDestroy() {
+    this.unsubChannelList();
+  }
+
+  subChannelList() {
+    return onSnapshot(this.getChannelRef(), channelList => {
+      this.channels = [];
+      channelList.forEach(channel => {
+        console.log(this.toJson(channel.data(), channel.id));
+        this.channels.push(this.toJson(channel.data(), channel.id));
+      })
+    })
+  }
+
+  toJson(obj: any, id: string): Channel {
+    return {
+      docId: id,
+      name: obj.name,
+      description: obj.description,
+      owner: obj.owner,
+    };
+  }
+
+  getChannelRef() {
+    return collection(this.firestore, "channels");
   }
 }
