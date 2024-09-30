0eff773c2c2ea7b84c32aded9d834ae05c57de35
Alicja K <akwiaton@gmail.com>
2024-09-30 16:35:34 +0200

    change channel name and change channel description input added to update-channel-dialog comp

--- src/app/channel-list/update-channel-dialog/update-channel-dialog.component.ts
+++ src/app/channel-list/update-channel-dialog/update-channel-dialog.component.ts
@@ -8,6 +8,8 @@ import {
   MatDialogContent,
   MatDialogTitle,
 } from '@angular/material/dialog';
+import { ChannelService } from '../../services/channel.service';
+import { Channel } from '../../interfaces/channel.interface';
 
 @Component({
   selector: 'app-update-channel-dialog',
@@ -19,4 +21,34 @@ import {
 })
 export class UpdateChannelDialogComponent {
 
+  isChannelNameHidden = false;
+  isChannelDescriptionHidden = false;
+
+  channelName: string = '';
+  channelDescription: string = '';
+
+  constructor(private channelService: ChannelService, private dialog: MatDialog) {}
+
+  getChannelList(): Channel[] {
+    return this.channelService.channels;
+  }
+
+  updateChannelName() {
+    this.isChannelNameHidden = !this.isChannelNameHidden;
+  }
+  changeChannelName() {
+    //change->upadate channelName
+    console.log('Channel-Name gespeichert');
+    this.isChannelNameHidden = false;
+
+  }
+
+  updateChannelDescription() {
+    this.isChannelDescriptionHidden = !this.isChannelDescriptionHidden;
+  }
+  changeChannelDescription() {
+    //change->upadate channelDescription
+    console.log('Channel-Description gespeichert');
+    this.isChannelDescriptionHidden = false;
+  }
 }
