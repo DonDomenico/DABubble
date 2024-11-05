683dd47e5e38a7d126c5f06d2ddf059abd66383e
Alicja K <akwiaton@gmail.com>
2024-11-05 12:02:29 +0100

    single message params uid works

--- src/app/chat/single-message/single-message.component.ts
+++ src/app/chat/single-message/single-message.component.ts
@@ -4,34 +4,63 @@ import { CommonModule } from '@angular/common';
 import { ConversationsService } from '../../services/conversations.service';
 import { UsersComponent } from '../../users/users.component';
 import { UserService } from '../../services/users.service';
-import {User} from '../../users/user.interface'
+import { User } from '../../users/user.interface';
 import { ShowProfileDialogComponent } from '../../users/show-profile-dialog/show-profile-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
-
-
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  Firestore,
+  onSnapshot,
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-single-message',
   standalone: true,
   imports: [MatIconModule, CommonModule, UsersComponent],
   templateUrl: './single-message.component.html',
-  styleUrl: './single-message.component.scss'
+  styleUrl: './single-message.component.scss',
 })
 export class SingleMessageComponent implements OnInit {
   @Output() toggleSingleMessage: EventEmitter<any> = new EventEmitter();
-  constructor(private conversationService: ConversationsService, private userServices: UserService, private dialog: MatDialog, private route: ActivatedRoute) {}
-  
-  userId: string = "";
+  constructor(
+    private conversationService: ConversationsService,
+    private userService: UserService,
+    private dialog: MatDialog,
+    private route: ActivatedRoute,
+    private firestore: Firestore
+  ) {}
 
-  ngOnInit() {
-    this.route.params.subscribe( async params => {
-      this.userId = params['id'];
-      console.log('GOT ID', this.userId);
-  })
-
+  userId: string = '';
+  user: User | undefined;
+  ngOnInit(): void {
+    this.route.children[0].params.subscribe(async (params) => {
+      this.userId = params['id'] || '';
+      this.getSingleUser();
+      console.log('GOT USER: ', this.userId);
+    });
   }
 
+  async getSingleUser() {
+    if (this.userId) {
+      const userRef = this.userService.getSingleUserRef(this.userId);
+      const userDoc = await getDoc(userRef);
+      if (userDoc.exists()) {
+        this.user = this.userService.toJson(userDoc.data(), userDoc.id);
+      } else {
+        console.log('No such document!');
+      }
+    } else {
+      this.user = this.userService.users[2];
+    }
+  }
+
+  getUserInfo(): User[] {
+    return this.userService.users;
+  }
   showProfileDialog() {
     this.dialog.open(ShowProfileDialogComponent);
   }
