cf41e1a1e8a1a1a58e91c9eb54a356e6d0d61c1f
DonDomenico <d.grunow@hotmail.de>
2025-02-12 16:54:12 +0100

    no changes

--- src/app/channel-list/single-channel/single-channel.component.ts
+++ src/app/channel-list/single-channel/single-channel.component.ts
@@ -345,4 +345,10 @@ export class SingleChannelComponent implements OnInit, OnDestroy {
       messageBoxContainer.style.width = '55%';
     }
   }
+
+  // window.addEventListener('resize', () => {
+  //   // We execute the same script as before
+  //   let vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', );
+  // });
 }
