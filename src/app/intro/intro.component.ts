1bdd949a274e1078b3afb165a26df0d98873d618
Alicja K <akwiaton@gmail.com>
2025-03-17 11:37:55 +0100

    several changes in CSS, working on intro

---
+++ src/app/intro/intro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  standalone: true,
+  imports: [],
+  templateUrl: './intro.component.html',
+  styleUrl: './intro.component.scss'
+})
+export class IntroComponent {
+
+
+}
